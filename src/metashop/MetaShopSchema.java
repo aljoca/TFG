d33c388b7dfe424f87f2a3e12af0b483e2b16fef
aljoc <joseantonio.martinez23@um.es>
2023-03-14 17:50:33 +0100

    [FEAT-1] Refactorizadas clases GraphSchema y MetaShopSchema

--- src/metashop/MetaShopSchema.java
+++ src/metashop/MetaShopSchema.java
@@ -1,12 +1,57 @@
 package metashop;
 
 import metashop.schema.graphdatamodel.model.GraphSchema;
+import org.neo4j.driver.*;
+import org.neo4j.driver.Record;
 
-public class MetaShopSchema{
-    public static void main(String... args) throws Exception {
-            // Creo graphSchema, que es quien se encargará de realizar las conexiones a la BBDD
-        try (GraphSchema graphSchema = new GraphSchema("MetaShop")) {
-            System.out.println(graphSchema);
+import java.util.ArrayList;
+
+public abstract class MetaShopSchema implements AutoCloseable{
+    private static Driver driver;
+    private final static String uri = "bolt://localhost:7687";
+    private final static String user = "neo4j";
+    private final static String password = "12345678";
+
+
+    /**
+     * Método para la obtención de todas las etiquetas existentes en la BBDD
+     * @return ArrayList de nodos
+     */
+    private static ArrayList<Record> getNodes() {
+        try (Session session = driver.session()) {
+            ArrayList<Record> nodes = session.executeWrite(tx -> {
+                Query query = new Query("MATCH (n) RETURN n");
+                Result result = tx.run(query);
+                return new ArrayList<>(result.list());
+            });
+            return nodes;
         }
     }
+
+    /**
+     * Método para la obtención de todas las relaciones existentes en la BBDD
+     * @return ArrayList de relaciones
+     */
+    private static ArrayList<Record> getRelationships(){
+        try (Session session = driver.session()) {
+            ArrayList<Record> relationships = session.executeWrite(tx -> {
+                Query query = new Query("MATCH (n)-[r]->() RETURN r");
+                Result result = tx.run(query);
+                return new ArrayList<>(result.list());
+            });
+            return relationships;
+        }
+    }
+
+    @Override
+    public void close() throws Exception {
+        driver.close();
+    }
+
+    public static void main(String... args) throws Exception {
+        driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password));
+        GraphSchema graphSchema = new GraphSchema("MetaShop", getNodes(), getRelationships());
+        System.out.println(graphSchema);
+    }
+
 }
--- src/metashop/schema/graphdatamodel/model/GraphSchema.java
+++ src/metashop/schema/graphdatamodel/model/GraphSchema.java
@@ -5,78 +5,37 @@ import org.neo4j.driver.Record;
 
 import java.util.ArrayList;
 
-public class GraphSchema implements AutoCloseable{
+public class GraphSchema{
 
     private final String name;
     private ArrayList<EntityType> entities;
     private ArrayList<RelationshipType> relationships;
-    private static Driver driver;
-    private final static String uri = "bolt://localhost:7687";
-    private final static String user = "neo4j";
-    private final static String password = "12345678";
 
-    public GraphSchema(String name) {
-        driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password));
+
+    public GraphSchema(String name, ArrayList<Record> nodes, ArrayList<Record> relationships) {
         this.name = name;
-        this.entities = addEntities();
-        this.relationships = addRelationships();
+        this.entities = addEntities(nodes);
+        this.relationships = addRelationships(relationships);
     }
 
-    public ArrayList<EntityType> addEntities(){
+    private ArrayList<EntityType> addEntities(ArrayList<Record> nodes){
         ArrayList<EntityType> entityTypes = new ArrayList<>();
-        ArrayList<Record> nodes = getNodes();
-        for (Record node : nodes) {
+        nodes.forEach((Record node) -> {
             EntityType entityType = new EntityType(node);
             if (!entityTypes.contains(entityType))
                 entityTypes.add(entityType);
-        }
+        });
         return entityTypes;
     }
 
-    public ArrayList<RelationshipType> addRelationships(){
+    private ArrayList<RelationshipType> addRelationships(ArrayList<Record> relationships){
         ArrayList<RelationshipType> relationshipTypes = new ArrayList<>();
-        ArrayList<Record> relationships = getRelationships();
-        for (Record relationship : relationships) {
+        relationships.forEach((Record relationship) -> {
             RelationshipType relationshipType = new RelationshipType(relationship);
             if (!relationshipTypes.contains(relationshipType))
                 relationshipTypes.add(relationshipType);
-        }
+        });
         return relationshipTypes;
-    }
-
-    /**
-     * Método para la obtención de todas las etiquetas existentes en la BBDD
-     * @return ArrayList de nodos
-     */
-    public static ArrayList<Record> getNodes() {
-        try (Session session = driver.session()) {
-            ArrayList<Record> nodes = session.executeWrite(tx -> {
-                Query query = new Query("MATCH (n) RETURN n");
-                Result result = tx.run(query);
-                return new ArrayList<>(result.list());
-            });
-            return nodes;
-        }
-    }
-
-    /**
-     * Método para la obtención de todas las relaciones existentes en la BBDD
-     * @return ArrayList de relaciones
-     */
-    public static  ArrayList<Record> getRelationships(){
-        try (Session session = driver.session()) {
-            ArrayList<Record> relationships = session.executeWrite(tx -> {
-                Query query = new Query("MATCH (n)-[r]->() RETURN r");
-                Result result = tx.run(query);
-                return new ArrayList<>(result.list());
-            });
-            return relationships;
-        }
-    }
-
-    @Override
-    public void close() throws Exception {
-        driver.close();
     }
 
     @Override
